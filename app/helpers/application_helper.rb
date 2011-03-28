451bd84244346ce63f854c897052c35916569d76
Yuu Imamura <reduced.residue@gmail.com>
2011-03-28 12:53:08 +0900

    Introduced Blueprint CSS into the public/stylesheets/, and modified application.html.erb to include it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def title
+    # Return a title on a per-page basis.
+    base_title = 'Ruby on Rails Tutorial; sample_app'
+    if @title.nil?
+      base_title
+    else
+      "#{@title} | #{base_title}"
+    end
+  end
 end
